6d07b11f6c275a11706b791909241e92f9f6c8c9
AndreLarson <dre.larson7@gmail.com>
2022-01-11 20:14:39 -0800

    added a despawn timer to dead knights

--- knight.js
+++ knight.js
@@ -4,8 +4,9 @@ class Knight {
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/knightDefault.png");
         this.spritesheetDead = ASSET_MANAGER.getAsset("./sprites/knightDead.png");
         this.animations = [];
+        this.deathAnimationTime = 1.2;
         this.animations[0] = new Animator(this.spritesheet, 0, 0, 41, 49, 6, 0.1, 23, false, true);
-        this.animations[1] = new Animator(this.spritesheetDead, 0, 0, 34, 50, 6, 0.2, 30, false, false);
+        this.animations[1] = new Animator(this.spritesheetDead, 0, 0, 34, 50, 6, this.deathAnimationTime / 6, 30, false, false);
         this.x = x;
         this.y = y;
         this.scale = 2.5;
@@ -21,11 +22,27 @@ class Knight {
     };
 
     update() {
+        if (this.dead) {
+            this.deadCounter += this.game.clockTick;
+            if (this.deadCounter > 5 + this.deathAnimationTime) this.removeFromWorld = true;
+        }
         if (this.x <= 0) this.x = 0;
         if (this.x + this.width >= PARAMS.CANVAS_WIDTH) this.x = PARAMS.CANVAS_WIDTH - this.width;
         if (this.y <= 0) this.y = 0;
         if (this.y + this.height >= PARAMS.CANVAS_HEIGHT) this.y = PARAMS.CANVAS_HEIGHT - this.height;
         this.updateBB();
+        var that = this;
+        this.game.entities.forEach(function (entity) {
+            if (entity.BB && that.BB.collide(entity.BB)) {
+                if (entity instanceof Knight) {
+                    if ((that.BB.bottom < entity.BB.bottom && that.game.isBehind(that, entity)) ||
+                        (that.BB.bottom >= entity.BB.bottom && !that.game.isBehind(that, entity))) {
+                        that.game.swapEntity(that, entity);
+                    }
+                    that.updateBB();
+                }
+            }
+        });
     };
 
     draw(ctx) {
